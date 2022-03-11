cd126c7f76f2576428e9918a9825efa7dff4b9d1
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-11 04:03:31 -0800

    6lack

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
+printf("size of an int: %lu byte(s)\n",(unsigne long)sizeof(i));
+printf("size of long int: %lu byte(s)\n",(unsigned long )sizeof(i2));
+printf("size of a long long: %lu byte(s)\n",(unsigned long)sizeof(i3));
+printf("size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
+return (0);
+}
