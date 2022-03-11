ec1713930c69913f4a57130816d4cd69979cf645
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-11 04:36:14 -0800

    Ambitious

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ float f;
 
 printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
 printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
-printf("size of long int: %lu byte(s)\n",(unsigned long )sizeof(i2));
+printf("size of a long int: %lu byte(s)\n",(unsigned long )sizeof(i2));
 printf("size of a long long: %lu byte(s)\n",(unsigned long)sizeof(i3));
-printf("size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
+printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 return (0);
 }
