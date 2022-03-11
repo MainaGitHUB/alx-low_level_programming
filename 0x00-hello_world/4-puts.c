3a033638f254c8e0862c549d60ad535cf6505c6d
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-11 03:28:06 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * C-programming
+ * Printing out command; puts
+ * /
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
