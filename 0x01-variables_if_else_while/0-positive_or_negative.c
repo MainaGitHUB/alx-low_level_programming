3e5cd2c0d234f669e8768082b9664648fa170833
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-13 01:27:03 -0800

    Commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,12 +11,12 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 printf("%d is positive\n", n);
-/*prints out positive if n is greater than zero*/
+/*if the number is greater than 0: is positive*/
 else if (n == 0)
 printf("%d is zero\n", n);
-/*prints out zero*/
+/*if the number is 0: is zero*/
 else
 printf("%d is negative\n", n);
-/*prints out negative if n is less than zero*/
+/*if the number is less than 0: is negative*/
 return (0);
 }
