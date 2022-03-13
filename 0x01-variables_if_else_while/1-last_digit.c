2b7ea7e11acfb227866397da686a2bafb0681271
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-13 03:30:03 -0700

    new commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+*/
+int main(void)
+{
+int n, lastDigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (lastDigit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n,  lastDigit);
+else if (lastDigit == 0)
+printf("Last digit od %d is %d and is 0\n", n, lastDigit);
+else if (lastDigit < 6 && lastDigit != 0)
+printf("Last digit of %d id %d and is less than 6 and ", n, lastDigit);
+printf("not 0\n");
+/* your code goes there */
+return (0);
+}
