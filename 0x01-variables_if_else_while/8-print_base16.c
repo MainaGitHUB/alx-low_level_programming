84e9bf9387f3f1a54c8088906a3d384b784cedb2
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-14 10:26:07 -0700

    bjon

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/* main - 
+ * return
+*/
 int main(void)
 {
 	int n;
@@ -9,7 +11,7 @@ int main(void)
 	{
 		putchar(n);
 	}
-	for (ch = 'a';ch <= 'f';ch++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
