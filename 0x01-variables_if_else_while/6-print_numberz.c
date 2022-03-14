8bac475e951ae93a3365b573e6bf86881d55163e
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-14 12:19:01 -0700

    Trevorreigns

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
