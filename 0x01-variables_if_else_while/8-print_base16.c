8777462feb9c8650ca9fae2ca34ffa719b410d5e
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-14 11:23:52 -0700

    vikings

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Program entry point
- * Return: 0 on success. Error code otherwise 
- */
+*main - Program entry point
+*Return:0 on success. Error code otherwise
+*/
 int main(void)
 {
 	int n;
