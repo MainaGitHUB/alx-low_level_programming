92db9962ccaffa0b699eeb1ab6e911b6782f8ea9
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-16 04:27:55 -0700

    Wednesday

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_last_digit - last digit of number
+ *@n: input.
+ *Return: last digit of number.
+ */
+int print_last_digit(int n)
+{
+	int lastDigit;
+	if (n >= 0)
+	{
+		lastDigit = n % 10;
+	}
+	else
+	{
+		lastDigit = (n % 10) * -1;
+	}
+	_putchar('0' + lastDigit);
+	return (lastDigit);
+}
