c3a4cc4be35d2209003820b6c05f9343851d0d4d
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-16 03:03:23 -0700

    Wednesday

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- *  *print_alphabet_x10 - print alpha x10
- *   *@void: no arguments.
- *    *
- *     *Return: no return value.
- *      */
+ * print_alphabet_x10 - print alpha x10
+ * @void: no arguments.
+ * Return: no return value.
+ */
 void print_alphabet_x10(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i, j;
-	i = 0;
 
+	i = 0;
 	while (i < 10)
 	{
 		j = 0;
+
 		while (j < 26)
 		{
 			_putchar(alphabet[j]);
