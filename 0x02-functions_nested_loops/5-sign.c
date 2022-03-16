3954c0a1ada2f1f0baa62bd2d74d4f0967b420f3
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-16 03:48:45 -0700

    Wednesday:

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- *  *print_sign - sign of a number.
- *   *@n: input.
- *    *
- *     *Return: 1 and print + if n > 1
- *      * 0 and print 0 if n = 0
- *       * -1 and print - if n < 0
- *        */
+ *print_sign - sign of a number.
+ *@n: input.
+ *Return: 1 and print + if n > 1
+ *0 and print 0 if n = 0
+ *-1 and print - if n < 0
+ */
 int print_sign(int n)
 {
 	int result;
+
 	if (n > 0)
 	{
 		_putchar('+');
@@ -22,7 +22,7 @@ int print_sign(int n)
 	}
 	if (n < 0)
 	{
-		_putchar('_');
+		_putchar('-');
 		result = -1;
 	}
 	return (result);
