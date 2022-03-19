7fc5dfc7c76323841bbb00645961b89b95afef1a
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-19 09:01:16 -0700

    New commits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * print_numbers - prints the numbers from 0 to 9
+ *   *
+ *    * @: character to print
+ *     *
+ *      * Return: void
+ */
+void print-numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
