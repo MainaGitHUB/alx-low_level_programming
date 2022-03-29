e3a5a244b83937d77d1aec8a99c53a53e09e5025
MainaGitHUB <mainatrevor45@gmail.com>
2022-03-29 01:38:36 -0700

    New commits

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ char *cap_string(char *);
 char *leet(char *n);
 char *rot13(char *s);
 void print_number(int n);
-char *infinite_add(char *n1, char n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void rev_string(char *n);
 void print_buffer(char *b, int size);
 
